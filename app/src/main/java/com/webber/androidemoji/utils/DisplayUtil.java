76cafcb803d52b2aa9b9b7ac085658df9b3dc918
Webber <280162265@qq.com>
2018-01-25 11:20:21 +0800

    ADD

---
+++ app/src/main/java/com/webber/androidemoji/utils/DisplayUtil.java
@@ -0,0 +1,141 @@
+package com.webber.androidemoji.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.view.WindowManager;
+
+/**
+ *  Created by Webber
+ *  Describe: 获得屏幕相关的辅助类
+ */
+public class DisplayUtil {
+	/**
+	 * 将px值转换为dp值
+	 */
+	public static int px2dp(Context context, float pxValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (pxValue / scale + 0.5f);
+	}
+
+	/**
+	 * 将dp值转换为px值
+	 */
+	public static int dp2px(Context context, float dpValue) {
+		final float scale = context.getResources().getDisplayMetrics().density;
+		return (int) (dpValue * scale + 0.5f);
+	}
+
+	/**
+	 * 将px值转换为sp值
+	 */
+	public static int px2sp(Context context, float pxValue) {
+		final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+		return (int) (pxValue / fontScale + 0.5f);
+	}
+
+	/**
+	 * 将sp值转换为px值
+	 */
+	public static int sp2px(Context context, float spValue) {
+		final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+		return (int) (spValue * fontScale + 0.5f);
+	}
+
+	/**
+	 * 获取屏幕宽度
+	 */
+	public static int getScreenWidthPixels(Activity context) {
+		DisplayMetrics metric = new DisplayMetrics();
+		context.getWindowManager().getDefaultDisplay().getMetrics(metric);
+		return metric.widthPixels;
+	}
+
+	/**
+	 * 获取屏幕高度
+	 */
+	public static int getScreenHeightPixels(Activity context) {
+		DisplayMetrics metric = new DisplayMetrics();
+		context.getWindowManager().getDefaultDisplay().getMetrics(metric);
+		return metric.heightPixels;
+	}
+
+	/**
+	 * 获取分辨率
+	 */
+	public static int getScreenResolution(Context context)
+	{
+		WindowManager wm = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		wm.getDefaultDisplay().getMetrics(outMetrics);
+		int width = outMetrics.widthPixels;
+		int height = outMetrics.heightPixels;
+		return width*height;
+	}
+
+	/**
+	 * 获得状态栏的高度
+	 */
+	public static int getStatusHeight(Context context)
+	{
+
+		int statusHeight = -1;
+		try
+		{
+			Class<?> clazz = Class.forName("com.android.internal.R$dimen");
+			Object object = clazz.newInstance();
+			int height = Integer.parseInt(clazz.getField("status_bar_height")
+					.get(object).toString());
+			statusHeight = context.getResources().getDimensionPixelSize(height);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return statusHeight;
+	}
+
+	/**
+	 * 获取当前屏幕截图，包含状态栏
+	 */
+	public static Bitmap snapShotWithStatusBar(Activity activity)
+	{
+		View view = activity.getWindow().getDecorView();
+		view.setDrawingCacheEnabled(true);
+		view.buildDrawingCache();
+		Bitmap bmp = view.getDrawingCache();
+		int width = getScreenWidthPixels(activity);
+		int height = getScreenHeightPixels(activity);
+		Bitmap bp = null;
+		bp = Bitmap.createBitmap(bmp, 0, 0, width, height);
+		view.destroyDrawingCache();
+		return bp;
+
+	}
+
+	/**
+	 * 获取当前屏幕截图，不包含状态栏
+	 */
+	public static Bitmap snapShotWithoutStatusBar(Activity activity)
+	{
+		View view = activity.getWindow().getDecorView();
+		view.setDrawingCacheEnabled(true);
+		view.buildDrawingCache();
+		Bitmap bmp = view.getDrawingCache();
+		Rect frame = new Rect();
+		activity.getWindow().getDecorView().getWindowVisibleDisplayFrame(frame);
+		int statusBarHeight = frame.top;
+
+		int width = getScreenWidthPixels(activity);
+		int height = getScreenHeightPixels(activity);
+		Bitmap bp = null;
+		bp = Bitmap.createBitmap(bmp, 0, statusBarHeight, width, height
+				- statusBarHeight);
+		view.destroyDrawingCache();
+		return bp;
+
+	}
+}
